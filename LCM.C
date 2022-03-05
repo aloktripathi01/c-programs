14c84d33f723d077c2cce1794da98d4eb1b0a1df
Alok Tripathi <54761280+aloktripatihi01@users.noreply.github.com>
2022-03-05 09:57:46 +0530

    Update LCM.C

--- LCM.C
+++ LCM.C
@@ -3,14 +3,14 @@
 void main()
 {
 int n,n1,max;
-printf("ENTER THE NUMBERS FOR FINDING LCM",n,n1);
+printf("Enter the  numbers for finding LCM",n,n1);
 scanf("%d%d",&n,&n1);
 max=(n>n1)?n:n1;
 while(1)
 {
 if(max%n==0&&max%n1==0)
 {
-printf("lcm=%d",max);
+printf("LCM=%d",max);
 break;
 }
 else
